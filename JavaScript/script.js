8d4230aa6656fb61d9f9bc440bd17c1f7332ce9c
derangee <daksh_vashishtha@srmap.edu.in>
2024-09-15 23:35:17 +0530

    updated scrollbar and plant cards

---
+++ JavaScript/script.js
@@ -0,0 +1,20 @@
+async function fetchcplants() {
+    let a = await fetch("");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let plants = [];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".svg")) {
+            plants.push(element.href);
+        }
+    }
+    return plants;
+}
+
+
+async main() {
+
+}
